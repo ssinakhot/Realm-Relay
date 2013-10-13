566ecb672ee73d5039e926e4ec00e4c5588613d5
DeVoidCoder <devoidcoder@gmail.com>
2013-10-13 15:29:13 -0500

    added GroundData.java for data from Tile.xml

---
+++ src/realmrelay/data/GroundData.java
@@ -0,0 +1,13 @@
+package realmrelay.data;
+
+public class GroundData {
+	
+	public int type = -1; // attribute type
+	public int maxDamage = -1; // element MaxDamage
+	public int minDamage = -1; // element MinDamage
+	public boolean noWalk = false; // element NoWalk
+	public boolean push = false; // element Push
+	public boolean sink = false; // element Sink
+	public float speed = 0; // element Speed
+
+}
